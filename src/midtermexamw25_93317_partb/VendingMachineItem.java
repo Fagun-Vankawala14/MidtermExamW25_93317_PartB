8d85af3ed3dc152d7c29f07061c23c9dcdb18ed7
Khaled Hreseh <hreseh@sheridancollege.ca>
2025-02-19 14:33:06 -0500

    adding new feature

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -27,9 +27,49 @@ public class VendingMachineItem {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
+    
+    //added by khaled Hreseh
+    
+    public static void addItem(String candy, double price, boolean availability) {
+    int n = candies.length;
+    // Check for capacity in arrays
+    if (n < 100) { // Assuming max 100 items for simplicity
+        // Extend arrays by 1
+        String[] newCandies = new String[n + 1];
+        double[] newPrices = new double[n + 1];
+        boolean[] newAvailability = new boolean[n + 1];
+        
+        System.arraycopy(candies, 0, newCandies, 0, n);
+        System.arraycopy(prices, 0, newPrices, 0, n);
+        System.arraycopy(itemAvailability, 0, newAvailability, 0, n);
+        
+        newCandies[n] = candy;
+        newPrices[n] = price;
+        newAvailability[n] = availability;
+        
+        candies = newCandies;
+        prices = newPrices;
+        itemAvailability = newAvailability;
+        
+        System.out.println("New item added successfully!");
+    } else {
+        System.out.println("No more space to add new items.");
+    }
+    //end
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
+        System.out.println("Enter 'add' to add a new item.");
+        String command = sc.nextLine();
+        if ("add".equalsIgnoreCase(command)) {
+            System.out.println("Enter candy name:");
+            String candy = sc.nextLine();
+            System.out.println("Enter price:");
+            double price = sc.nextDouble();
+            System.out.println("Enter availability (Yes/No):");
+            boolean availability = sc.nextBoolean();
+            addItem(candy, price, availability);
       }
+    }
 }
