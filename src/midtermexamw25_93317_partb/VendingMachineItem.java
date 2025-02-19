888fc599491c2843d58a26c3e19951310f326d0e
Lucas Pasqualoni <pasqualu@sheridancollege.ca>
2025-02-19 13:38:49 -0500

    Second Commit for the midterm Code, Question 22. Completed the Parameterized Constructor.

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class VendingMachineItem {
     public double price;
+    public String newItem;
     
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
     public static String[] newItems = {"Item One","Item Two"};
 
-    public VendingMachineItem(double price) {
+    public VendingMachineItem(double price , String newItem) {
         this.price = price;
+        this.newItem = newItem;
     }
     
     public static String[] getCandies() {
