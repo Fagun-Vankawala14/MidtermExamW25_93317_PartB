6a94102b209aaefe1763539eb7aba7bbef85c812
Harmanpreet Kaur <kau17997@sheridancollege.ca>
2025-02-19 13:51:22 -0500

    Completed the parameterized constructor in given code

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -8,27 +8,27 @@ public class VendingMachineItem {
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
-    
-    public VendingMachineItem() {
-        // Constructor left blank intentionally
+
+    public VendingMachineItem(double price) {
+        this.price = price; 
     }
-    
+
     public double getPrice() {
         return price;
     }
-    
+
     public void setPrice(double givenPrice) {
         price = givenPrice;
     }
-    
+
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < candies.length; i++) {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
-    
-        public static void addItem(String name, double price) {
+
+    public static void addItem(String name, double price) {
         int newSize = candies.length + 1;
         String[] newCandies = new String[newSize];
         double[] newPrices = new double[newSize];
@@ -48,22 +48,22 @@ public class VendingMachineItem {
 
         System.out.println("Item added successfully: " + name + " - $" + price);
     }
-       
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-        
+
         System.out.println("Do you want to add a new item? (yes/no)");
         String response = sc.next();
         if (response.equalsIgnoreCase("yes")) {
             System.out.print("Enter item name: ");
-            sc.nextLine(); // Consume newline
+            sc.nextLine();
             String itemName = sc.nextLine();
             System.out.print("Enter item price: ");
             double itemPrice = sc.nextDouble();
             addItem(itemName, itemPrice);
         }
-        
+
         displayMenu();
     }
 }
