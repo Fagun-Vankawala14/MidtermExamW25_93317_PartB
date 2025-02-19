51d8bbdfacce9066ad7d782f0d48086f2ce71529
Austin Sinclair <sinclaau@sheridancollege.ca>
2025-02-19 14:14:19 -0500

    Added new ArrayLists for modifiable candies and prices. Left the old lists just in case, but will probably be using the new ArrayLists in the future. Modified display method to include new items as well.

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -1,16 +1,29 @@
 package midtermexamw25_93317_partb;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class VendingMachineItem {
     public double price;
     
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
+    public static ArrayList<String> moreCandies = new ArrayList<>();
+    
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
+    public static ArrayList<Double> morePrices = new ArrayList<>();
+    
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
+    public static ArrayList<Boolean> moreAvailability = new ArrayList<>();
     
     public VendingMachineItem() {
         // Constructor left blank intentionally
+    }
+    
+    public static void addItem(String candy, double price) {
+        moreCandies.add(candy);
+        morePrices.add(price);
+        moreAvailability.add(true);
     }
     
     public double getPrice() {
@@ -26,10 +39,15 @@ public class VendingMachineItem {
         for (int i = 0; i < 4; i++) {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
+        for (int i = 0; i < moreCandies.size(); i++) {
+            System.out.println((i+5) + ". " + moreCandies.get(i) + " - $" + morePrices.get(i));
+        }
     }
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        addItem("Nerdz", 1.00);
+        addItem("Jujubes", 3.00);
         displayMenu();
       }
 }
