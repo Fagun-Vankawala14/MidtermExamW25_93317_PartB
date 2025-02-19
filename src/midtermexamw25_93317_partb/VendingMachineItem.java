3c76464674fe855d3fc2ce03652433d9163106b7
Divish Tuteja <tutejad@sheridancollege.ca>
2025-02-19 14:27:49 -0500

    new feature for admin to add new item in inventory

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -1,16 +1,25 @@
 package midtermexamw25_93317_partb;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class VendingMachineItem {
     public double price;
     
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
-    public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
+    public static List<String> candies = new ArrayList<>(Arrays.asList("chocolate bar", "sour candy", "soft drink", "potato chips"));
+    public static List<Double> prices = new ArrayList<>(Arrays.asList(1.50, 1.20, 1.80, 2.00));
+    public static List<Boolean> itemAvailability = new ArrayList<>(Arrays.asList(true, true, true, true));
     
     public VendingMachineItem() {
         // Constructor left blank intentionally
+    }
+    
+    public static void addItem(String candy, double price, boolean available) {
+        candies.add(candy);
+        prices.add(price);
+        itemAvailability.add(available);
     }
     
     public double getPrice() {
@@ -23,13 +32,13 @@ public class VendingMachineItem {
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
+        for (int i = 0; i < candies.size(); i++) {
+            System.out.println((i+1) + ". " + candies.get(i) + " - $" + prices.get(i));
         }
     }
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-      }
+    }
 }
