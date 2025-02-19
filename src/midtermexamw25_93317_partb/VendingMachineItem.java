03410c5416754ad27f1da834141d1c103581bbbe
Divish Tuteja <tutejad@sheridancollege.ca>
2025-02-19 14:31:44 -0500

    added customer item selection

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -11,7 +11,7 @@ public class VendingMachineItem {
     public static List<String> candies = new ArrayList<>(Arrays.asList("chocolate bar", "sour candy", "soft drink", "potato chips"));
     public static List<Double> prices = new ArrayList<>(Arrays.asList(1.50, 1.20, 1.80, 2.00));
     public static List<Boolean> itemAvailability = new ArrayList<>(Arrays.asList(true, true, true, true));
-    
+
     
     public VendingMachineItem(double price) {
         this.price = price;
@@ -37,9 +37,23 @@ public class VendingMachineItem {
             System.out.println((i+1) + ". " + candies.get(i) + " - $" + prices.get(i));
         }
     }
+    
+    public static void selectItem(Scanner sc) {
+        System.out.println("Enter the item number to select:");
+        int choice = sc.nextInt();
+        if (choice < 1 || choice > candies.size()) {
+            System.out.println("Invalid selection!");
+        } else if (itemAvailability.get(choice - 1)) {
+            System.out.println("Item dispensed: " + candies.get(choice - 1));
+            itemAvailability.set(choice - 1, false);
+        } else {
+            System.out.println("Item out of stock!");
+        }
+    }
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
+        selectItem(sc);
     }
 }
