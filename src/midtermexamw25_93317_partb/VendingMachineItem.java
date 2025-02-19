8d89f50cd01eb86d24071678ca8d838f933fa813
Anamika <anamika1@sheridancollege.ca>
2025-02-19 13:47:49 -0500

    Add the parameterized constructor in VendingMachineItem class.

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class VendingMachineItem {
     public double price;
     
+    public VendingMachineItem(double price) {
+    this.price = price;
+}
+
+    
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
