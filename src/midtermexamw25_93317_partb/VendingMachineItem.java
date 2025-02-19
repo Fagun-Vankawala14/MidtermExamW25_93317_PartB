a0a1330e5119174f7acde349de508d3650106b52
shahin <kashahin@sheridancollege.ca>
2025-02-19 14:05:39 -0500

    allowing admin to add items

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -27,9 +27,51 @@ public class VendingMachineItem {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
+    
+    // method to allow admin to add new items
+    public static void addItem(String itemName, double itemPrice, boolean isAvailable) {
+        int newLength = candies.length + 1;
+        String[] newCandies = new String[newLength];
+        double[] newPrices = new double[newLength];
+        boolean[] newAvailability = new boolean[newLength];
+
+        for (int i = 0; i < candies.length; i++) {
+            newCandies[i] = candies[i];
+            newPrices[i] = prices[i];
+            newAvailability[i] = itemAvailability[i];
+        }
+
+        newCandies[newLength - 1] = itemName;
+        newPrices[newLength - 1] = itemPrice;
+        newAvailability[newLength - 1] = isAvailable;
+
+        candies = newCandies;
+        prices = newPrices;
+        itemAvailability = newAvailability;
+
+        System.out.println("Item added successfully!");
+    }
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-      }
+        
+    // admin can add new data
+        System.out.println("\nAs admin, would you like to add a new item? (yes/no)");
+        String response = sc.nextLine();
+        if (response.equalsIgnoreCase("yes")) {
+            System.out.println("Enter item name:");
+            String newItemName = sc.nextLine();
+            System.out.println("Enter item price:");
+            double newItemPrice = sc.nextDouble();
+            System.out.println("Is the item available? (true/false):");
+            boolean newItemAvailability = sc.nextBoolean();
+            addItem(newItemName, newItemPrice, newItemAvailability);
+        }
+
+        // this will display updated menu
+        displayMenu();
+
+    }
+      
 }
