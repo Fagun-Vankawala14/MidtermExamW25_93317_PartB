80da4c9031d4f1c39ce0071a38dbe2dc3308fc98
Lovepreet kaur <Lovepr41@sheridancollege.ca>
2025-02-19 14:11:21 -0500

    "Completed parameterized constructor in Item class"

---
+++ src/midtermexamw25_93317_partb/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexamw25_93317_partb;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Creating an Item object using the parameterized constructor
+        Item item1 = new Item("Laptop", 1200.99, 10);
+        
+        // Display the details of the item
+        System.out.println(item1);
+    }
+}
+
