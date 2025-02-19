e4643aefc8a6e88ae5b36787bee58dba8e8d9c82
Anamika <anamika1@sheridancollege.ca>
2025-02-19 13:46:04 -0500

    Added feature for admin to add new item in inventory.

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -27,6 +27,26 @@ public class VendingMachineItem {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
+    
+    public void addNewItem(String name, double price) {
+    
+    String[] newCandies = new String[candies.length + 1];
+    double[] newPrices = new double[prices.length + 1];
+    boolean[] newAvailability = new boolean[itemAvailability.length + 1];
+    
+    System.arraycopy(candies, 0, newCandies, 0, candies.length);
+    System.arraycopy(prices, 0, newPrices, 0, prices.length);
+    System.arraycopy(itemAvailability, 0, newAvailability, 0, itemAvailability.length);
+    
+    newCandies[candies.length] = name;
+    newPrices[prices.length] = price;
+    newAvailability[itemAvailability.length] = true;
+    
+    candies = newCandies;
+    prices = newPrices;
+    itemAvailability = newAvailability;
+}
+
        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
