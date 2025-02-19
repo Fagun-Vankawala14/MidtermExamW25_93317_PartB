c3dc2c4256a2b2346b0379d59795b3916b499e5b
Krishna Sharma <shar1601@sheridancollege.ca>
2025-02-19 14:21:24 -0500

    Added feature for admin to add new item to the vending machine

--- src/midtermexamw25_93317_partb/VendingMachineItem.java
+++ src/midtermexamw25_93317_partb/VendingMachineItem.java
@@ -23,13 +23,54 @@ public class VendingMachineItem {
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < candies.length; i++) {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
-       
+
+    // Add a new item to the inventory
+    public static void addNewItem(String itemName, double itemPrice, boolean availability) {
+        // Create new arrays with extra space for the new item
+        String[] newCandies = new String[candies.length + 1];
+        double[] newPrices = new double[prices.length + 1];
+        boolean[] newItemAvailability = new boolean[itemAvailability.length + 1];
+        
+        // Copy existing items to new arrays
+        System.arraycopy(candies, 0, newCandies, 0, candies.length);
+        System.arraycopy(prices, 0, newPrices, 0, prices.length);
+        System.arraycopy(itemAvailability, 0, newItemAvailability, 0, itemAvailability.length);
+
+        // Add the new item to the arrays
+        newCandies[candies.length] = itemName;
+        newPrices[prices.length] = itemPrice;
+        newItemAvailability[itemAvailability.length] = availability;
+        
+        // Update the static arrays with the new ones
+        candies = newCandies;
+        prices = newPrices;
+        itemAvailability = newItemAvailability;
+
+        System.out.println("New item added: " + itemName);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-      }
+
+        // Admin adds a new item
+        System.out.println("\nAdmin: Would you like to add a new item? (yes/no)");
+        String adminResponse = sc.nextLine();
+        if (adminResponse.equalsIgnoreCase("yes")) {
+            System.out.println("Enter item name:");
+            String itemName = sc.nextLine();
+            System.out.println("Enter item price:");
+            double itemPrice = sc.nextDouble();
+            System.out.println("Is the item available? (true/false)");
+            boolean itemAvailable = sc.nextBoolean();
+            addNewItem(itemName, itemPrice, itemAvailable);
+        }
+
+        // Display updated menu after adding the new item
+        displayMenu();
+    }
 }
